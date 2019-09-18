a61ee98c936bf720786d75bc7680dc31b1a7bcc1
MaximillianoNico <maximillianonico8@gmail.com>
2019-09-18 11:48:33 +0700

    update page

---
+++ pages/news/index.js
@@ -0,0 +1,12 @@
+export default ({ value = 'Unit Test' }) => {
+    return (
+        <div>
+            News Page {value}
+            <div style={{ display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+                <div className="form-group__container">
+                    <input className=""/>
+                </div>
+            </div>
+        </div>
+    )
+}
